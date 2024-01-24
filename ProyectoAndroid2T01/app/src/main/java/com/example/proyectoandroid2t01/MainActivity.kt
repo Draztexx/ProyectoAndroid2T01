ca92d99b4867dab5bf6ebd5e884d381a23f4628f
Draztexx <adnetluna@gmail.com>
2024-01-24 14:54:45 +0100

    V.0.0.3

--- ProyectoAndroid2T01/app/src/main/java/com/example/proyectoandroid2t01/MainActivity.kt
+++ ProyectoAndroid2T01/app/src/main/java/com/example/proyectoandroid2t01/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.proyectoandroid2t01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var correo :EditText
     lateinit var password:EditText
     lateinit var loginButton: Button
-    lateinit var registerButton:Button
+    lateinit var registerButton:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         correo=findViewById(R.id.correo)
         password=findViewById(R.id.Password)
-        loginButton=findViewById(R.id.logignbutton)
-        registerButton=findViewById(R.id.registerbutton)
+        loginButton=findViewById(R.id.loginbutton)
+        registerButton=findViewById(R.id.redirectregister)
 
         loginButton.setOnClickListener(clickListener)
         registerButton.setOnClickListener(clickListener)
 
-
-
-
-
     }
 
     private val clickListener= View.OnClickListener { view->
-
+        when(view.id){
+            R.id.loginbutton->{
+                AlertDialog.Builder(this)
+                    .setTitle("Inicio")
+                    .setMessage("Botton funciona")
+                    .setPositiveButton("Aceptar"){dialog,_->}.show()
+            }
+            R.id.redirectregister->{
+                startActivity(Intent(this@MainActivity,Register::class.java));
+            }
+        }
     }
 }
--- ProyectoAndroid2T01/app/src/main/java/com/example/proyectoandroid2t01/Register.kt
+++ ProyectoAndroid2T01/app/src/main/java/com/example/proyectoandroid2t01/Register.kt
@@ -1,11 +1,64 @@
 package com.example.proyectoandroid2t01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class Register : AppCompatActivity() {
+
+    lateinit var nombre :EditText
+    lateinit var edad   :EditText
+    lateinit var correo :EditText
+    lateinit var password   :EditText
+    lateinit var loginButton: Button
+    lateinit var redirectButton:TextView
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        nombre=findViewById(R.id.nombre)
+        edad=findViewById(R.id.edad)
+        correo=findViewById(R.id.correo)
+        password=findViewById(R.id.password)
+        loginButton=findViewById(R.id.loginbutton)
+        redirectButton=findViewById(R.id.redirectlogin)
+
+        loginButton.setOnClickListener(clickListener)
+        redirectButton.setOnClickListener(clickListener)
+
+
     }
+
+    private val clickListener= View.OnClickListener { view->
+        when(view.id){
+            R.id.loginbutton->{
+                AlertDialog.Builder(this)
+                    .setTitle("Register")
+                    .setMessage("Botton funciona")
+                    .setPositiveButton("Aceptar"){dialog,_->}.show()
+            }
+
+            R.id.redirectlogin->{
+                startActivity(Intent(this@Register,MainActivity::class.java))
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
 }
