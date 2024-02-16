a966d63853c7eaeffce998b504a8bb0c3be59a5b
Draztexx <102992060+Draztexx@users.noreply.github.com>
2024-02-16 20:30:24 +0100

    vprueba

--- ProyectoAndroid2T01/app/build.gradle.kts
+++ ProyectoAndroid2T01/app/build.gradle.kts
@@ -55,9 +55,10 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.gms:play-services-maps:17.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
